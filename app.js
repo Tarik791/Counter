bd7a08c4263bcb7039bfcd00b5e1b634a3d98414
Tarik791 <72981197+Tarik791@users.noreply.github.com>
2022-06-07 12:22:51 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+
+    btn.addEventListener('click', function(e){
+
+        const styles = e.currentTarget.classList;
+
+        //if we click decrease, number goes down
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('increase'))
+        {
+            count ++;
+        }
+        else{
+
+            count = 0;
+
+        }
+        if(count > 0){
+
+            value.style.color = 'green';
+
+        }
+        if(count < 0){
+
+            value.style.color = 'red';
+        }
+        if(count === 0){
+
+            value.style.color = "#222"; 
+        
+        }
+
+        value.textContent = count;
+
+    });
+});
